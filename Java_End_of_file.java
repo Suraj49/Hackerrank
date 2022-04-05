3b8df1e88ca333f610d2c2dfb143a753605f8259
Suraj49 <1507surajkumar@gmail.com>
2022-04-05 19:45:46 +0530

    Commits

---
+++ Java_End_of_file.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Java_End_of_file {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+        while (sc.hasNext()) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+    }
+}
